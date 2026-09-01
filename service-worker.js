9cfd67ecf4d1edca96bc773bef745d0614667cd8
Pelomatrix10 <yezz.ers34@gmail.com>
2026-09-01 00:14:57 -0500

    Add secure beta admin panel

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "tito-github-v5-auth";
+const CACHE_NAME = "tito-github-v6-admin";
 const BASE = self.registration.scope;
 
 const APP_SHELL = [
@@ -58,3 +58,4 @@ self.addEventListener("fetch", (event) => {
       )
   );
 });
+
